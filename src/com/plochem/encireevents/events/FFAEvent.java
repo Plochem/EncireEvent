a0a49c57ba076acb88dcfe30d56df43d9ee4c26a
Ryan Chu <plochemisswag@gmail.com>
2019-10-18 21:49:13 -0700

    Create FFAEvent.java

---
+++ src/com/plochem/encireevents/events/FFAEvent.java
@@ -0,0 +1,19 @@
+package com.plochem.encireevents.events;
+
+import com.plochem.encireevents.Event;
+
+public class FFAEvent extends Event{
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
