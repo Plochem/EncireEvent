579743bc939cce28b9098e6cdee7abec64c1b644
Ryan Chu <plochemisswag@gmail.com>
2019-10-24 00:08:23 -0700

    check if there is event

--- src/com/plochem/encireevents/MenuListener.java
+++ src/com/plochem/encireevents/MenuListener.java
@@ -1,6 +1,7 @@
 package com.plochem.encireevents;
 
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -10,29 +11,38 @@ import com.plochem.encireevents.events.FFAEvent;
 
 
 public class MenuListener implements Listener{
-	
+
 	private EncireEvent eventPlugin;
-	
+
 	public MenuListener(EncireEvent eventPlugin) {
 		this.eventPlugin = eventPlugin;
 	}
-	
+
 	@EventHandler
 	public void onInvClick(InventoryClickEvent e) {
 		if(e.getInventory().getName().equalsIgnoreCase(eventPlugin.getMenuConfig().getString("menu-name"))) {
 			e.setCancelled(true);
-			ConfigurationSection items = eventPlugin.getMenuConfig().getConfigurationSection("menu-items");
-			ItemStack i = e.getCurrentItem();
-			if(items.getItemStack("ffa.item").equals(i)) {
-				eventPlugin.setEvent(new FFAEvent());
-			} else if(items.getItemStack("waterdrop.item").equals(i)) {
-				
-			} else if(items.getItemStack("sumo.item").equals(i)) {
-				
-			} else if(items.getItemStack("temperature.item").equals(i)) {
-				
-			} else if(items.getItemStack("islandclash.item").equals(i)) {
-				
+			Player p = (Player)e.getWhoClicked();
+			if(e.getClickedInventory().getName().equalsIgnoreCase(eventPlugin.getMenuConfig().getString("menu-name"))) {
+				ConfigurationSection items = eventPlugin.getMenuConfig().getConfigurationSection("menu-items");
+				ItemStack i = e.getCurrentItem();
+				if(eventPlugin.getEvent() != null) {
+					eventPlugin.sendMsg(p, eventPlugin.getMessageonfig().getString("event-already-exists"));
+				} else {
+					if(items.getItemStack("ffa.item").equals(i)) {
+						eventPlugin.setEvent(new FFAEvent());
+					} else if(items.getItemStack("waterdrop.item").equals(i)) {
+
+					} else if(items.getItemStack("sumo.item").equals(i)) {
+
+					} else if(items.getItemStack("temperature.item").equals(i)) {
+
+					} else if(items.getItemStack("islandclash.item").equals(i)) {
+
+					}
+					eventPlugin.sendMsg(p, eventPlugin.getMessageonfig().getString("event-created"));
+				}
+
 			}
 		}
 
