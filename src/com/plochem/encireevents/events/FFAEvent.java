2b747f7b97fbf93f4c2e31fc38d7ec177d0072f0
Ryan Chu <plochemisswag@gmail.com>
2019-11-02 23:41:30 -0700

    give items

--- src/com/plochem/encireevents/events/FFAEvent.java
+++ src/com/plochem/encireevents/events/FFAEvent.java
@@ -4,8 +4,10 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
-import com.plochem.encireevents.EncireEvent;
 import com.plochem.encireevents.Event;
 
 public class FFAEvent extends Event{
@@ -20,8 +22,18 @@ public class FFAEvent extends Event{
 	@Override
 	public void start() {
 		for(UUID id : this.getPlayers()) {
-			Bukkit.getPlayer(id).teleport(startLoc);
+			Player p = Bukkit.getPlayer(id);
+			p.teleport(startLoc);
+			p.getInventory().setArmorContents(new ItemStack[] {new ItemStack(Material.IRON_BOOTS), 
+															 	new ItemStack(Material.IRON_LEGGINGS), 
+															 	new ItemStack(Material.IRON_CHESTPLATE),
+															 	new ItemStack(Material.IRON_HELMET),});
+			p.getInventory().addItem(new ItemStack(Material.IRON_SWORD), 
+									new ItemStack(Material.BOW), 
+									new ItemStack(Material.GOLDEN_APPLE, 10),
+									new ItemStack(Material.ARROW, 16));
 		}
+		
 	}
 
 	@Override
