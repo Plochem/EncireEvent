6484cbffc3b5d987c92b4c31b9384daf549f64bd
Ryan Chu <plochemisswag@gmail.com>
2019-11-02 23:41:54 -0700

    player death handle

--- src/com/plochem/encireevents/listeners/PlayerDamageEvent.java
+++ src/com/plochem/encireevents/listeners/PlayerDamageEvent.java
@@ -4,21 +4,37 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 import com.plochem.encireevents.EncireEvent;
 import com.plochem.encireevents.Event;
 
 public class PlayerDamageEvent implements Listener{
+	
+	private EncireEvent plugin = EncireEvent.plugin;
+	
 	@EventHandler
 	public void onDamage(EntityDamageByEntityEvent e) {
-		EncireEvent plugin = EncireEvent.plugin;
 		if(e.getEntity() instanceof Player && e.getDamager() instanceof Player) { //player dmg player
 			Event event = plugin.getEvent();
-			if(event.isSpectator(e.getEntity().getUniqueId()) && event.isSpectator(e.getDamager().getUniqueId())) {
-				
-			} else if(event.isPlayer(e.getEntity().getUniqueId()) && event.isPlayer(e.getDamager().getUniqueId())) {
-				
+			if(event==null) return;
+			if(event.isSpectator(e.getEntity().getUniqueId()) && event.isSpectator(e.getDamager().getUniqueId())) { // if both spectator -> no dmg
+				e.setCancelled(true);
+			} else if(event.isPlayer(e.getEntity().getUniqueId()) && event.isPlayer(e.getDamager().getUniqueId())) { // if both player in event
+				if(!event.hasStarted()) {
+					e.setCancelled(true);
+				}
 			}
+		}
+	}
+	
+	@EventHandler
+	public void onDamage(PlayerDeathEvent e) {
+		Event event = plugin.getEvent();
+		if(event==null) return;
+		if(event.isPlayer(e.getEntity().getUniqueId())) {
+			e.getEntity().teleport(event.getSpecLocation());
+			event.playerToSpecator(e.getEntity().getUniqueId());
 		}
 	}
 
