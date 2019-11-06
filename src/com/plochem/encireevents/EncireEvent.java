6741e2f2d1eda18d8d49975492258a3ed0945ebb
Ryan Chu <plochemisswag@gmail.com>
2019-11-06 00:21:24 -0800

    set game spawns for island clash event

--- src/com/plochem/encireevents/EncireEvent.java
+++ src/com/plochem/encireevents/EncireEvent.java
@@ -2,9 +2,13 @@ package com.plochem.encireevents;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
@@ -24,7 +28,7 @@ public class EncireEvent extends JavaPlugin{
 	private YamlConfiguration hostMenu;
 	private File eventFile = new File(this.getDataFolder(), "event-config.yml");
 	private YamlConfiguration eventConfig;
-	
+
 	public void onEnable() {
 		plugin = this;
 		getLogger().info("____________________________");
@@ -34,17 +38,17 @@ public class EncireEvent extends JavaPlugin{
 		registerThings();
 		createFiles();
 	}
-	
+
 	public void registerThings() {
 		PluginManager pm = Bukkit.getPluginManager();
 		pm.addPermission(new Permission("events.reload"));
 		pm.addPermission(new Permission("events.host"));
 		pm.addPermission(new Permission("events.spectatorspawn"));
 		pm.addPermission(new Permission("events.gamespawn"));
-		
+
 		pm.registerEvents(new MenuListener(this), this);
 	}
-	
+
 	public void createFiles() {
 		if(!messageFile.exists()) {
 			messageFile.getParentFile().mkdirs();
@@ -62,7 +66,7 @@ public class EncireEvent extends JavaPlugin{
 		}
 		eventConfig = YamlConfiguration.loadConfiguration(eventFile);
 	}
-	
+
 	public void save(File f, YamlConfiguration c) {
 		try {
 			c.save(f);
@@ -70,7 +74,8 @@ public class EncireEvent extends JavaPlugin{
 			e.printStackTrace();
 		}
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
 		if(command.getName().equalsIgnoreCase("events")) {
@@ -140,10 +145,37 @@ public class EncireEvent extends JavaPlugin{
 						}
 					} else if(args[0].equalsIgnoreCase("gamespawn")) {
 						if(p.hasPermission("events.gamespawn")) {
-							if(args[1].equalsIgnoreCase("ffa")) {
-								eventConfig.set("ffa-startLoc", p.getLocation());
-								save(eventFile, eventConfig);
-								p.sendMessage(msgFormat(messages.getString("set-new-gamespawn")));
+							if(args.length >= 2) {
+								if(args[1].equalsIgnoreCase("ffa")) {
+									eventConfig.set("ffa-startLoc", p.getLocation());
+									save(eventFile, eventConfig);
+									p.sendMessage(msgFormat("&cYou set the location of the game spawn for the FFA event to your current position."));	
+								} else if(args[1].equalsIgnoreCase("islandclash") || args[1].equalsIgnoreCase("sumo")) { //TODO
+									if(args.length == 3) { // /events gamespawn islandclash 1
+										if(StringUtils.isNumeric(args[2]) && Integer.parseInt(args[2]) > 0) {
+											int idx = Integer.parseInt(args[2]);
+											List<Location> locs = (List<Location>)eventConfig.getList("islandclash-startLoc");
+											if(locs == null) locs = new ArrayList<>();
+											if(locs.size() >= idx) {
+												locs.set(idx-1, p.getLocation());
+												p.sendMessage(msgFormat("&cYou set the location of game spawn #" + idx + " for the Island Clash event to your current position."));		
+											} else {
+												locs.add(p.getLocation());	
+												p.sendMessage(msgFormat("&cYou're trying to set game spawn #" + idx + ", but the previous ones have not been created. So you just set the location of game spawn #" + locs.size() + " for the Island Clash event to your current position."));											 
+											}
+											eventConfig.set("islandclash-startLoc", locs);
+										} else {
+											p.sendMessage(msgFormat("&cUse a positive integer to specify the game spawn."));
+										}
+									} else {
+										p.sendMessage(msgFormat("&cYou need to specify an which game spawn you want to set."));
+									}
+								} else {
+									p.sendMessage(msgFormat("&ePossible choices: ffa, temperature, islandclash, sumo, or waterdrop"));	
+								}
+								
+							} else {
+								p.sendMessage(msgFormat("&cYou need to specify which event you want to modify the game spawns."));
 							}
 						} else {
 							p.sendMessage(msgFormat(messages.getString("no-permission-message")));	
@@ -156,7 +188,7 @@ public class EncireEvent extends JavaPlugin{
 		}
 		return false;
 	}
-	
+
 	private void openHostingMenu(Player p) {
 		int size = hostMenu.getInt("menu-size");
 		String name = hostMenu.getString("menu-name");
@@ -173,37 +205,37 @@ public class EncireEvent extends JavaPlugin{
 			p.sendMessage(msgFormat(s));
 		}
 	}
-	
+
 	public Event getEvent() {
 		return event;
 	}
-	
+
 	public void setEvent(Event event) {
 		this.event = event;
 	}
-	
+
 	public YamlConfiguration getMenuConfig() {
 		return hostMenu;
 	}
-	
+
 	public YamlConfiguration getMessageConfig() {
 		return messages;
 	}	
-	
+
 	public YamlConfiguration getEventConfig() {
 		return eventConfig;
 	}
-	
-	
+
+
 	public void reload() {
 		messages = YamlConfiguration.loadConfiguration(messageFile);
 		hostMenu = YamlConfiguration.loadConfiguration(hostMenuFile);
 		eventConfig = YamlConfiguration.loadConfiguration(eventFile);
 	}
-	
+
 	public String msgFormat(String s) {
 		return ChatColor.translateAlternateColorCodes('&', s).replaceAll("%event%", event.getName());
 	}
-	
-	
+
+
 }
