80c04a13c672efe9b9de3064e42896266ceb82f5
Ryan Chu <plochemisswag@gmail.com>
2019-11-07 00:45:16 -0800

    start island clash event

--- src/com/plochem/encireevents/events/IslandClashEvent.java
+++ src/com/plochem/encireevents/events/IslandClashEvent.java
@@ -1,19 +1,28 @@
 package com.plochem.encireevents.events;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import com.plochem.encireevents.Event;
 
 public class IslandClashEvent extends Event{
+	
+	private List<Location> startLoc;
 
-	public IslandClashEvent(String name, int maxPlayers, Location specLoc) {
+	public IslandClashEvent(String name, int maxPlayers, Location specLoc, List<Location> startLoc) {
 		super(name, maxPlayers, specLoc);
+		this.startLoc = startLoc;
 	}
 
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-		
+		for(int i = 0; i < this.getPlayers().size(); i++) {
+			Player p = Bukkit.getPlayer(this.getPlayers().get(i));
+			p.teleport(startLoc.get(i));
+		}
 	}
 	
 	@Override
